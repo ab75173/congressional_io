dfb4b3f8811ed8bb78569145fd7f07151f503a60
ab75173 <adam.p.barrett@gmail.com>
2014-05-29 21:10:21 -0400

    removed migration files

--- db/migrate/20140528194054_remove_fiedname_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFiednameFromUser < ActiveRecord::Migration
-  def down
-    remove_column :users, :fieldname
-  end
-end
--- db/migrate/20140528202815_remove_fieldnameandmember_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFieldnameandmemberFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :member, :string
-    remove_column :users, :fieldname, :string
-  end
-end
